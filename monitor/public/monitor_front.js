f8c981a760799699f6e4b872351d7f4327231f6b
JohanGil1502 <johan.gil@uptc.edu.co>
2024-11-19 21:44:17 -0500

    Métodos para  deplegar un servidor y cambiar el estado del un servidor

--- monitor/public/monitor_front.js
+++ monitor/public/monitor_front.js
@@ -8,7 +8,8 @@ new Vue({
         { id: 1, status: true, isLeader: true, ip: 'localhost', port: 40600 }
         ],
         ipMonitorBack: '192.168.1.2',
-        portMonitorBack: 7000
+        portMonitorBack: 7000,
+        id: 0
     },
     methods: {
         // Método para conectarse por websockets al back del cliente
@@ -26,6 +27,37 @@ new Vue({
             this.socket.on('serversList', (data) => {
                 this.serversList = data;
             });
+        },
+        async deploy(){
+            const response = await fetch(`http://${this.ipMonitorBack}:${this.portMonitorBack}/deploy`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id: this.id})
+            });
+    
+            const data = await response.json();
+            
+            if (data.answer === 'OK') {
+                console.log('servidor desplegado');
+            } else {
+                console.log('no fue posible desplegar el servidor');
+            }
+        },
+        async changeServerStatus(){
+            const response = await fetch(`http://${this.ipMonitorBack}:${this.portMonitorBack}/changeServerStatus`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ ip: ip, port: port})
+            });
+    
+            const data = await response.json();
+            
+            if (data.answer === 'OK') {
+                console.log('se ha cambiado el estado del servidor');
+            } else {
+                console.log('no fue posible cambiado el estado del servidor');
+            }
+
         }
     },
     mounted() {
