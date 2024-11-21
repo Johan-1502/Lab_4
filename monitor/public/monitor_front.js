9e98826b3ec5ec1b6788e426938535994eeff7b3
SebasGrosso <sebastian.gil01@uptc.edu.co>
2024-11-21 14:23:36 -0500

    Interfaz del monitor

--- monitor/public/monitor_front.js
+++ monitor/public/monitor_front.js
@@ -1,14 +1,16 @@
 new Vue({
     el: '#app',
     data: {
-        serversList: [{id:1, status:true, isLeader:true, ip:'localhost', port:4000},
-            {id:1, status:true, isLeader:true, ip:'localhost', port:4111},
-            {id:1, status:false, isLeader:true, ip:'localhost', port:40300},
-            {id:1, status:true, isLeader:true, ip:'localhost', port:40050},
-            {id:1, status:true, isLeader:true, ip:'localhost', port:40600}
+        serversList: [{id:1, status:true, isLeader:false, ip:'localhost', port:4000},
+            {id:1, status:true, isLeader:false, ip:'localhost', port:4001},
+            {id:1, status:false, isLeader:false, ip:'localhost', port:4002},
+            {id:1, status:true, isLeader:true, ip:'localhost', port:4003},
+            {id:1, status:true, isLeader:false, ip:'localhost', port:4004},
+            {id:1, status:true, isLeader:false, ip:'localhost', port:4004}
         ],
         ipMonitorBack: '192.168.171.202',
-        portMonitorBack: 7000
+        portMonitorBack: 7000,
+        newServerModal: false
     },
     methods: {
         // Método para conectarse por websockets al back del cliente
@@ -21,8 +23,14 @@ new Vue({
             this.socket.on('serversList', (data) => {
                 this.serversList = data; 
             });
-
-        }
+        },
+        openModal(){
+            this.newServerModal = true;
+        },
+        closeModal(){
+            this.newServerModal = false;
+        },
+        
     },
     mounted() {
         this.socket();
